0e1ac5ffb48969e2ff1745cd66a0980dd3f887e8
Mostafa <mostafa.m.qasemi@gmail.com>
2026-05-14 20:19:45 +0330

    Add files via upload

---
+++ Day 08 function_with_inputs.py
@@ -0,0 +1,12 @@
+def great():
+    print("Hello")
+    print("How do you do?")
+    print("Isn't the weather nice?")
+
+great()
+
+def great_with_name(name, location):
+    print(f"Hello {name}")
+    print(f"Where is {location} ?")
+
+great_with_name("ali", "tehran")
